e0d291f4ded1f79df07d7026fdf1f4f566669b99
partouf <partouf@gmail.com>
2016-03-15 17:20:43 +0100

    Create raspicam_still_interface.cpp

---
+++ src/raspicam_still_interface.cpp
@@ -0,0 +1,9 @@
+#include "raspicam_still_interface.h"
+
+raspicam::RaspiCam_Still_Interface::RaspiCam_Still_Interface()
+{
+}
+
+raspicam::RaspiCam_Still_Interface::~RaspiCam_Still_Interface()
+{
+}
